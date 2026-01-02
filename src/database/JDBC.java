d356fc4a8138dc82f30fcf4a7f2c4534194611ba
Kavindiya-madubashani <HK smart@DESKTOP-4734EG0.city_campus>
2026-01-02 12:33:55 +0530

    First Commit

---
+++ src/database/JDBC.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+/**
+ *
+ * @author HK smart
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class JDBC {
+    
+    private Connection con;
+    
+    public Connection  getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/library_db",
+                    "root",
+                    "Kavi2006@"
+            );
+            
+            System.out.println("Database connected Successfully");
+            
+            }
+        
+        catch(ClassNotFoundException | SQLException e){
+            JOptionPane.showMessageDialog(null, "Database Connection Failed: "+ e.getMessage());
+            e.printStackTrace();
+        }
+        return con;
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+    
+}
+
